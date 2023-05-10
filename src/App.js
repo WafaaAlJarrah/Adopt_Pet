4959a713d4d895773f60762f0f737056c70d7918
eliekf <eliekfoury00@gmail.com>
2023-05-10 11:29:52 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
 import Login from "./Components/Login/Login.js";
 import Signup from "./Components/SignUp/Signup.js";
 import { useState } from "react";
@@ -7,15 +10,19 @@ import { useState } from "react";
 function App() {
   // const [IsSigup, setIsSigup] = useState(false);
   return (
-    
-      <div className="App">
-        <Routes>
-          {/* <Route path="/" element={!IsSigup ? <Signup /> : <Login />}></Route> */}
-          <Route path="/Login" element={<Login />}></Route>
-          <Route path="/SignUp" element={<Signup />}></Route>
-        </Routes>
-      </div>
-    
+
+    <div className="App">
+      <Routes>
+        < Navbar />
+        {/* <Route path="/" element={!IsSigup ? <Signup /> : <Login />}></Route> */}
+        <Route path="/" element={<Home />} />
+        <Route path="#about" />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/SignUp" element={<Signup />}></Route>
+      </Routes>
+    </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./Redux/store/ReduxStore";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
