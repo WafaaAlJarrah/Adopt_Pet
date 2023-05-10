d2e3974d1a4ef07b951ea02f22cd0b45d4d9357b
eliekf <eliekfoury00@gmail.com>
2023-05-10 11:07:58 +0300

    Home Layout

---
+++ src/Components/Modals/ModalD.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import DogsModal from './DogsModal';
+import   '../../App.css';
+function ModalPop() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button onClick={handleShow} style={{backgroundColor: "#333", color: "#fff", fontSize: "18px"}}>
+        View Dogs
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">Available Types of Dogs</Modal.Title>
+        </Modal.Header>
+        <Modal.Body> < DogsModal/></Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+export default ModalPop;
