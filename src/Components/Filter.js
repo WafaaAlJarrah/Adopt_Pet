4be4e2de949a2a4a092b4ee30897c0188ec0ef33
eliekf <eliekfoury00@gmail.com>
2023-06-13 12:22:08 +0300

    Filtering

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,102 +1,156 @@
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
-function ButtonDropdownsExample() {
+export default function SelectSmall() {
+    // const [age, setAge] = React.useState('');
+
+    // const handleChange = (event) => {
+    //     setAge(event.target.value);
+    // };
+
     return (
-        <>
-            <h1> Filter by </h1><br></br>
-            <InputGroup className="mb-3">
-                <DropdownButton
-                    variant="outline-secondary"
-                    title="Filter by Type"
-                    id="input-group-dropdown-1"
+        <div className='Filtering'>
+            <FormControl sx={{ m: 1, minWidth: 150 }} >
+                <InputLabel id="demo-simple-select-autowidth-label">Type</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-select-small"
+                    // value={age}
+                    label="Age"
+                    // onChange={handleChange}
                 >
-                    <Dropdown.Item href="#">Action</Dropdown.Item>
-                    <Dropdown.Item href="#">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#">Something else here</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#">Separated link</Dropdown.Item>
-                </DropdownButton>
-                <Form.Control aria-label="Text input with dropdown button" />
-            </InputGroup>
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl><br></br>
+            <FormControl sx={{ m: 1, minWidth: 150 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">Age</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-select-small"
+                    // value={age}
+                    label="Age"
+                    // onChange={handleChange}
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl> <br></br>
+            <FormControl sx={{ m: 1, minWidth: 150 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">Size</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-select-small"
+                    // value={age}
+                    label="Age"
+                    // onChange={handleChange}
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl><br></br>
+            <FormControl sx={{ m: 1, minWidth: 150 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">Gender</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-select-small"
+                    // value={age}
+                    label="Age"
+                    // onChange={handleChange}
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl><br></br>
+            <FormControl sx={{ m: 1, minWidth: 150 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">Color</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-select-small"
+                    // value={age}
+                    label="Age"
+                    // onChange={handleChange}
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl><br></br>
+            <FormControl sx={{ m: 1, minWidth: 150 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">Price</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-select-small"
+                    // value={age}
+                    label="Age"
+                    // onChange={handleChange}
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl><br></br>
+            <FormControl sx={{ m: 1, minWidth: 150 }} >
+                <InputLabel id="demo-simple-select-autowidth-label">Location</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-select-small"
+                    // value={age}
+                    label="Age"
+                    // onChange={handleChange}
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl><br></br>
+            <FormControl sx={{ m: 1, minWidth: 150 }} className='FormControl' >
+                <InputLabel id="demo-simple-select-autowidth-label">Name</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-select-small"
+                    // value={age}
+                    label="Age"
+                    // onChange={handleChange}
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select><br></br>
+                <button> Search </button>
+            </FormControl>
+        </div>
 
-            <InputGroup className="mb-3">
-                <Form.Control aria-label="Text input with dropdown button" />
-
-                <DropdownButton
-                    variant="outline-secondary"
-                    title="Filter by Price"
-                    id="input-group-dropdown-1"
-                >
-                    <Dropdown.Item href="#">Type</Dropdown.Item>
-                    <Dropdown.Item href="#">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#">Something else here</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#">Separated link</Dropdown.Item>
-                </DropdownButton>
-            </InputGroup>
-            <InputGroup>
-                <DropdownButton
-                    variant="outline-secondary"
-                    title="Filter by Location"
-                    id="input-group-dropdown-1"
-                >
-                    <Dropdown.Item href="#">Price</Dropdown.Item>
-                    <Dropdown.Item href="#">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#">Something else here</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#">Separated link</Dropdown.Item>
-                </DropdownButton>
-                <Form.Control aria-label="Text input with 2 dropdown buttons" />
-            </InputGroup><br></br>
-            <InputGroup className="mb-3">
-                <DropdownButton
-                    variant="outline-secondary"
-                    title="Filter by Age"
-                    id="input-group-dropdown-1"
-                >
-                    <Dropdown.Item href="#">Action</Dropdown.Item>
-                    <Dropdown.Item href="#">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#">Something else here</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#">Separated link</Dropdown.Item>
-                </DropdownButton>
-                <Form.Control aria-label="Text input with dropdown button" />
-            </InputGroup>
-            <InputGroup className="mb-3">
-                <Form.Control aria-label="Text input with dropdown button" />
-
-                <DropdownButton
-                    variant="outline-secondary"
-                    title="Filter by Color"
-                    id="input-group-dropdown-1"
-                >
-                    <Dropdown.Item href="#">Type</Dropdown.Item>
-                    <Dropdown.Item href="#">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#">Something else here</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#">Separated link</Dropdown.Item>
-                </DropdownButton>
-            </InputGroup>
-            <InputGroup>
-                <DropdownButton
-                    variant="outline-secondary"
-                    title="Filter by Gender"
-                    id="input-group-dropdown-1"
-                >
-                    <Dropdown.Item href="#">Price</Dropdown.Item>
-                    <Dropdown.Item href="#">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#">Something else here</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#">Separated link</Dropdown.Item>
-                </DropdownButton>
-                <Form.Control aria-label="Text input with 2 dropdown buttons" />
-            </InputGroup>
-            <br></br>
-        </>
     );
 }
-
-export default ButtonDropdownsExample;
