e2e94152675c7f36859a7288d35cb63b5faecb2e
Wafaa Al Jarrah <wafaajarrah99@gmail.com>
2023-06-22 11:00:01 +0300

    allHistories

--- src/Components/HistoryCard.js
+++ src/Components/HistoryCard.js
@@ -5,12 +5,21 @@ function HistoryCard({ history }) {
   console.log("history ", history);
   const { requestInfo, status } = history;
   console.log("requestInfo ", requestInfo);
-  console.log("status ", status);
   return (
     <Card className="mb-4">
       <Card.Body>
         <Row>
-          <Col xs={8}>all histories</Col>
+          <Col xs={8}>
+            <h5>History status:</h5>
+            <p>{status}</p>
+          </Col>
+          <Col xs={8}>
+            <h5>Request info:</h5>
+            <p> Sender name: {requestInfo.sender.fullName}</p>
+            <p> Animal name: {requestInfo.animal.name}</p>
+            <p> Animal specification: {requestInfo.animal.specification.name}</p>
+            <p>Request message: {requestInfo.message}</p>
+          </Col>
         </Row>
       </Card.Body>
     </Card>
